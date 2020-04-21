ab142b09fbe1bf59e3deba8593c83a47676917ed
tobi7070 <54136828+tobi7070@users.noreply.github.com>
2020-04-21 14:56:47 +0200

    Revert "Revert "#24 Add Home Controller, #23 Add Home Views, #21 Add Indtity View, #1 Add Basic Identity""
    This reverts commit a8a5ee611ff3737f21d391cb64e425f288afe1fd.

---
+++ Data/InMemoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolNetwork.Data
+{
+    public class InMemoryContext : IdentityDbContext
+    {
+        public InMemoryContext(DbContextOptions<InMemoryContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
