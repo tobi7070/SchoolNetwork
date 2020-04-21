a8a5ee611ff3737f21d391cb64e425f288afe1fd
tobi7070 <54136828+tobi7070@users.noreply.github.com>
2020-04-21 14:56:20 +0200

    Revert "#24 Add Home Controller, #23 Add Home Views, #21 Add Indtity View, #1 Add Basic Identity"
    This reverts commit d01f422788928d86eda81eb4ccbecf8c7c0e0716.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,97 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SchoolNetwork.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly UserManager<IdentityUser> userManager;
-        private readonly SignInManager<IdentityUser> signInManager;
-
-        public HomeController(UserManager<IdentityUser> _userManager, SignInManager<IdentityUser> _signInManager)
-        {
-            userManager = _userManager;
-            signInManager = _signInManager;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult About()
         {
             return View();
         }
-
-        [Authorize]
-        public IActionResult Secret()
-        {
-            return View();
-        }
-
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Register(string username, string password)
-        {
-            var user = new IdentityUser
-            {
-                UserName = username,
-                Email = "",
-            };
-
-            var regsiterResult = await userManager.CreateAsync(user, password);
-
-            if (regsiterResult.Succeeded)
-            {
-                var signInResult = await signInManager.PasswordSignInAsync(username, password, false, false);
-
-                if (signInResult.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(string username, string password)
-        {
-            var user = await userManager.FindByNameAsync(username);
-
-            if (user != null)
-            {
-                var signInResult = await signInManager.PasswordSignInAsync(username, password, false, false);
-
-                if (signInResult.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
-
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await signInManager.SignOutAsync();
-            return RedirectToAction("Index");
-        }
-
     }
 }
